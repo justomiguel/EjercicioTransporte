c0fb62fb0d4e6e769f638b8e503d1ef3b9ef8783
Justo Miguel Vargas <justomiguelvargas@gmail.com>
2024-10-16 16:34:42 -0300

    Update Coche.java

--- micodigo/src/Coche.java
+++ micodigo/src/Coche.java
@@ -3,4 +3,9 @@ public class Coche extends MedioTransporte {
     public Coche() {
         setMultplicador(10);
     }
+
+    @Override
+    public void estacionar(){
+        System.out.println("Estacione el coche");
+    }
 }
